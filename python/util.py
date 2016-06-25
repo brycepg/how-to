6782a9a6c3a2ec82b4b687d3d9cf5800f3045426
Bryce Guinta <azrathud@gmail.com>
2016-06-25 12:44:13 -0600

    move reference.py to util.py; add some utility functions

--- python/util.py
+++ python/util.py
@@ -4,9 +4,16 @@ import os
 # then it will not work since __file__ is a relative path
 os.path.dirname(os.path.abspath(__file__))
 
-# Get all files in a directory
-# Never use os.walk again
 def all_sub_files(root):
+    """Get all files in a directory.
+
+    Never use os.walk again
+    """
     for path, subdirs, files in os.walk(root):
         for name in files:
             yield os.path.join(path, name)
+
+
+def lmap(*args, **kwargs):
+    """Take map generator and make a list for Python 3 work"""
+    return list(map(*args, **kwargs))
