2973b664e8c9cf551d5d7277ab4995125be5fad0
Bryce Guinta <azrathud@gmail.com>
2016-06-25 12:10:22 -0600

    Add util for not using os.walk

--- python/reference.py
+++ python/reference.py
@@ -3,3 +3,10 @@ import os
 # If you call this from the current directory without abspath,
 # then it will not work since __file__ is a relative path
 os.path.dirname(os.path.abspath(__file__))
+
+# Get all files in a directory
+# Never use os.walk again
+def all_sub_files(root):
+    for path, subdirs, files in os.walk(root):
+        for name in files:
+            yield os.path.join(path, name)
